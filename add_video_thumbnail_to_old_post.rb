ae95e1cf470b578b9ca18c61647da9706383044e
Gislene Pereira <gislene01@gmail.com>
2014-12-24 15:25:03 -0300

    Refatorando script dos video thumbnails

--- add_video_thumbnail_to_old_post.rb
+++ add_video_thumbnail_to_old_post.rb
@@ -4,50 +4,45 @@ require 'pry'
 require 'yaml'
 require 'video_info'
 
-repo = '/Users/jcsouzaa/Documents/projetos/site-novo/'
+def nil_or_empty var
+  var.nil? || var.empty?
+end
+
+def green_line(text)
+  puts "\e[32m #{text}"
+  print "\e[0m"
+end
+
+def red_line(text)
+  puts "\e[31m #{text}"
+  print "\e[0m"
+end
+
+repo = 'absolute/path/to/project/root/folder/'
 files = Dir["#{repo}_posts/**/*.md"]
 files.each do |file|
-    puts file
     raw = File.open(file).read
     raw = raw.split("\n---\n")
     body = raw[1]
     metadata = Psych.load(raw[0])
+    has_video = metadata.has_key?('video') ? !nil_or_empty(metadata['video']) : false
+    has_thumbnail = metadata.has_key?('video_thumbnail')
 
-    if metadata.has_key? 'video'
-      unless metadata['video'].nil? || metadata['video'].empty?
-        unless metadata.has_key? 'video_thumbnail'
-          begin
-            video = VideoInfo.new(metadata['video'])
-
-            metadata['video_thumbnail'] = video.thumbnail_large if video.available?
-          rescue VideoInfo::UrlError => e
-            puts e.message
-          end
+    if has_video && !has_thumbnail
+      puts "Processando arquivo: #{file}"
+      begin
+        video = VideoInfo.new(metadata['video'])
+        if video.available?
+          metadata['video_thumbnail'] = video.thumbnail_large
+          green_line "Thumbnail adicionado com sucesso ao vídeo #{metadata['video']}. Thumbnail: #{metadata['video_thumbnail']}"
+        else
+          red_line "Vídeo #{metadata['video']} indisponível."
         end
+      rescue VideoInfo::UrlError => e
+        red_line "Url do video incorreta. #{e.message}"
       end
     end
-
-    # puts "Possui vídeo = #{metadata.has_key? 'video'}"
-    # has_video = metadata.has_key? 'video' && !nil_or_empty(metadata['video'])
-    # has_thumbnail = metadata.has_key? 'video_thumbnail'
-    #
-    # if has_video && !has_thumbnail
-    #   puts "Entrou no if"
-    #   begin
-    #     video = VideoInfo.new(metadata['video'])
-    #
-    #     metadata['video_thumbnail'] = video.thumbnail_large if video.available?
-    #   rescue VideoInfo::UrlError => e
-    #     puts e.message
-    #   end
-    # end
 
     result = [Psych.dump(metadata),'---', body].join("\n")
     File.open(file, 'w') { |file| file.write(result) }
 end
-
-# def nil_or_empty var
-#   puts "Se variavel é nula: #{var.nil?}"
-#   puts "Se variavel está vazia: #{var.empty?}"
-#   var.nil? || var.empty?
-# end
